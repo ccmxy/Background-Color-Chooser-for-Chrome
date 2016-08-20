a43ca5ea34d362a98f4a8fcb8b2d8eebfb51531c
ccmxy <windrainicefire@gmail.com>
2016-08-19 19:06:29 -0700

    Lets user submit color of choice

--- popup/popup.js
+++ popup/popup.js
@@ -4,6 +4,26 @@ document.getElementById("btn").onclick = function(e){
   hello();
 }
 
+document.getElementById("colorButton").onclick = function(e){
+  changeColor();
+}
+
+function changeColor() {
+  var color = document.getElementById('color_input').value;
+  var number = Math.floor((Math.random() * 4) + 1);
+  var colorArray = ['red', 'blue', 'yellow', 'pink', 'orange'];
+
+  chrome.tabs.executeScript({
+  code: 'document.body.style.backgroundColor="' + color + '"'
+});
+  chrome.runtime.sendMessage({
+      greeting: "hello"
+    },
+    function(response) {
+      document.getElementById("div").textContent = response.msg;
+    });
+}
+
 function hello() {
   console.log("hello");
   var number = Math.floor((Math.random() * 4) + 1);
