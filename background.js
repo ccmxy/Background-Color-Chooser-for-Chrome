1cbd7ae367b99dfba3c99c6b876278e336435d7f
ccmxy <windrainicefire@gmail.com>
2016-08-19 18:40:04 -0700

    Random color each time u reload extension

--- background.js
+++ background.js
@@ -3,10 +3,16 @@
 	// found in the LICENSE file.
 
 	// Called when the user clicks on the browser action.
+	//
+	var number = Math.floor((Math.random() * 4) + 1);
+	var colorArray = ['red', 'blue', 'yellow', 'pink', 'orange'];
+	//
 	chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
 	  console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript({
-	    code: 'document.body.style.backgroundColor="red"'
+	    // code: 'document.body.style.backgroundColor="red"'
+			code: 'document.body.style.backgroundColor="' + colorArray[number] + '"'
+
 	  });
 	});
